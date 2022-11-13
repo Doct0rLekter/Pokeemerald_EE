6557978023343e93bd26c4c73c62f71ec75c1cb6
Doct0rLekter <115762567+Doct0rLekter@users.noreply.github.com>
2022-11-12 22:37:25 -0500

    Clean up default option comments

--- src/new_game.c
+++ src/new_game.c
@@ -96,9 +96,9 @@ static void SetDefaultOptions(void)
     gSaveBlock2Ptr->optionsBattleStyle = OPTIONS_BATTLE_STYLE_SET;
     gSaveBlock2Ptr->optionsBattleSceneOff = FALSE;
     gSaveBlock2Ptr->regionMapZoom = FALSE;
-    gSaveBlock2Ptr->optionsUnitSystem = 0;  //tx_optionsPlus
-    gSaveBlock2Ptr->optionsHpBarSpeed = 8;  //tx_optionsPlus
-    gSaveBlock2Ptr->optionsExpBarSpeed = 5; //tx_optionsPlus
+    gSaveBlock2Ptr->optionsUnitSystem = 0;          //tx_optionsPlus
+    gSaveBlock2Ptr->optionsHpBarSpeed = 8;          //tx_optionsPlus
+    gSaveBlock2Ptr->optionsExpBarSpeed = 5;         //tx_optionsPlus
     gSaveBlock2Ptr->optionsDisableMatchCall = 0;    //tx_optionsPlus
     gSaveBlock2Ptr->optionsCurrentFont = 0;         //tx_optionsPlus
 }
